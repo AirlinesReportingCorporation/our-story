277d16cec16426fe2990aa14dcb9c4c4cec29e75
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-03-09 17:25:44 -0500

    Begin Our Story Content Mapping

--- src/app.jsx
+++ src/app.jsx
@@ -11,10 +11,19 @@ class App extends Component {
   render() {
     return (
       <div style={{ width: "100%" }}>
+        <div className="container story-section1">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="our-story-title">
+                <span className="story-title-offset">Our</span><span className="story-title">Story</span>
+                <span className="story-title-hr"></span>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              Start Here
             </div>
           </div>
         </div>
