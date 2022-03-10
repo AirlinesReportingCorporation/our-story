dcbd8e673b2afd4e9ec12d169d7bb145f56303eb
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-03-10 14:31:53 -0500

    add Customer section and css

--- src/app.jsx
+++ src/app.jsx
@@ -76,17 +76,28 @@ class App extends Component {
                   <div className="story-section2-passenger">
                     <span className="story-section2-number">15</span>{" "}
                     <span className="story-section2-billion">Billion</span>{" "}
-                    <span className="story-section2-text" style={{marginLeft: "-158px"}}>
+                    <span
+                      className="story-section2-text"
+                      style={{ marginLeft: "-158px" }}
+                    >
                       Passenger Flights
                     </span>
                   </div>
                   <div className="story-section2-passenger">
                     <span className="story-section2-number">490</span>
-                    <span className="story-section2-text" style={{marginLeft: "-182px"}}>Airlines</span>
+                    <span
+                      className="story-section2-text"
+                      style={{ marginLeft: "-182px" }}
+                    >
+                      Airlines
+                    </span>
                   </div>
                   <div className="story-section2-passenger">
                     <span className="story-section2-number">23</span>{" "}
-                    <span className="story-section2-text" style={{marginLeft: "-149px"}}>
+                    <span
+                      className="story-section2-text"
+                      style={{ marginLeft: "-149px" }}
+                    >
                       Countries & Territories
                     </span>
                   </div>
@@ -102,16 +113,115 @@ class App extends Component {
         <div className="story-section3">
           <div className="container">
             <div className="row">
-              <div className="col-lg-12" style={{display: "inline-flex", justifyContent: "space-between"}}>
+              <div
+                className="col-lg-12"
+                style={{
+                  display: "inline-flex",
+                  justifyContent: "space-between",
+                }}
+              >
                 <div className="story-section3-item">
-                 <span className="story-section3-header">OUR VISION</span>
-                 <p className="story-section3-text">Enable the growth of global air travel to strengthen economies and enrich lives.</p>
+                  <span className="story-section3-header">OUR VISION</span>
+                  <p className="story-section3-text">
+                    Enable the growth of global air travel to strengthen
+                    economies and enrich lives.
+                  </p>
                 </div>
                 <div className="story-section3-item">
-                <span className="story-section3-header">OUR MISSION</span>
-                <p className="story-section3-text">Accelerate the growth of global air travel.</p>
+                  <span className="story-section3-header">OUR MISSION</span>
+                  <p className="story-section3-text">
+                    Accelerate the growth of global air travel.
+                  </p>
                 </div>
-
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="story-customers">
+          <div className="container" id="customers">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="story-customers-header story-header">
+                  <span className="customer-empower">How We Empower</span>
+                  <span className="story-preHeader">Our</span>{" "}
+                  <span className="story-mainHeader">Customers</span>
+                </div>
+                <div className="story-about-customers d-flex">
+                  <div className="story-customer-item col-6">
+                    <div>
+                    <img src="https://www2.arccorp.com/globalassets/about-us/our-story/Agent.png" alt="" className="story-customer-icon" />
+                    </div>
+                    <div className="story-customer-info">
+                      <span className="customer-header">
+                        Equipping Travel Agencies
+                      </span>
+                      <p className="customer-info">
+                        We help agencies build strong industry relationships,
+                        settle transactions with airlines and customers,
+                        seamlessly manage travel for their clients, and grow
+                        their businesses.
+                      </p>
+                      <a href="" className="customer-learn">
+                        Learn More
+                      </a>
+                    </div>
+                  </div>
+                  <div className="story-customer-item col-6">
+                    <div>
+                      <img src="https://www2.arccorp.com/globalassets/about-us/our-story/PLANE.png" alt="" className="story-customer-icon" />
+                    </div>
+                    <div className="story-customer-info">
+                      <span className="customer-header">
+                        Enabling Airline Success
+                      </span>
+                      <p className="customer-info">
+                        Our data, distribution and financial solutions help
+                        airlines settle transactions with travel agencies and
+                        corporations, grow their businesses, and increase
+                        profitability across all channels of distribution.
+                      </p>
+                      <a href="" className="customer-learn">
+                        Learn More
+                      </a>
+                    </div>
+                  </div>
+                  <div className="story-customer-item col-6">
+                    <div>
+                    <img src="https://www2.arccorp.com/globalassets/about-us/our-story/CORPORATE.png" alt="" className="story-customer-icon" />
+                    </div>
+                    <div className="story-customer-info">
+                      <span className="customer-header">
+                        Guiding Corporate Travel Programs
+                      </span>
+                      <p className="customer-info">
+                        We provide solutions and analytics to help corporations
+                        seamlessly engage with airlines across multiple channels
+                        and strategically manage their travel programs.
+                      </p>
+                      <a href="" className="customer-learn">
+                        Learn More
+                      </a>
+                    </div>
+                  </div>
+                  <div className="story-customer-item col-6">
+                    <div>
+                    <img src="https://www2.arccorp.com/globalassets/about-us/our-story/DATA.png" alt="" className="story-customer-icon" />
+                    </div>
+                    <div className="story-customer-info">
+                      <span className="customer-header">
+                        Providing High-Quality Travel Data
+                      </span>
+                      <p className="customer-info">
+                        ARC’s data reflects large-scale travel trends and the
+                        smallest details, enabling companies to target audiences
+                        and predict demand.
+                      </p>
+                      <a href="" className="customer-learn">
+                        Learn More
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
